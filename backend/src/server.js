0834f677885270e84ea4698ba35d015665fc18c0
Rasul <rasuleldo@gmail.com>
2025-09-25 06:36:57 +0300

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+// import cors from "cors"; // временно закомментируйте
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -7,10 +9,25 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+// app.use(cors()); // временно закомментируйте
+app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(3000, () => console.log("Server is running on port: " + PORT));
+// Production setup
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/\/(?!api).*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log("Server is running on port: " + PORT));
